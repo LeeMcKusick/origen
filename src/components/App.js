b8228e74abce7e2047176831d2778cbc52771261
Lee Skellington-McKusick <lee.mckusick@gmail.com>
2020-05-02 23:49:18 -0400

    Testing component inclusion

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
 import logo from '../logo.svg';
 import '../styles/App.css';
+import Backstory from './Backstory';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Origen</h1>
       </header>
+      <Backstory></Backstory>
     </div>
   );
 }
---
+++ src/components/Backstory.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Backstory extends React.Component {
+    render() {
+        return(
+            <h1>Backstory</h1>
+        )
+    }
+}
+
+export default Backstory;
